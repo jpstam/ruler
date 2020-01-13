7786c0f6a2671652d5a4a451e3dbb1ded8145471
Tijn Centen (MSI) <tijncenten@hotmail.com>
2020-01-13 11:51:37 +0100

    Update gridstrategy move placement along borders

--- unity/Assets/Scripts/Voronoi/Controller/Custom/Strategies/GridStrategy.cs
+++ unity/Assets/Scripts/Voronoi/Controller/Custom/Strategies/GridStrategy.cs
@@ -18,8 +18,14 @@ public class GridStrategy : Strategy {
 
         for (int x = 0; x < xOptions; x++) {
             for (int y = 0; y < yOptions; y++) {
-                float xCoord = Mathf.Lerp(gs.BottomLeft.x, gs.TopRight.x, x * 1f / (xOptions - 1)) + Random.value - 0.5f;
-                float yCoord = Mathf.Lerp(gs.BottomLeft.y, gs.TopRight.y, y * 1f / (yOptions - 1)) + Random.value - 0.5f;
+                float startX = 1f / (xOptions + 1) / 2;
+                float endX = 1 - startX;
+                float stepX = (endX - startX) / (xOptions - 1);
+                float startY = 1f / (yOptions + 1) / 2;
+                float endY = 1 - startY;
+                float stepY = (endY - startY) / (yOptions - 1);
+                float xCoord = Mathf.Lerp(gs.BottomLeft.x, gs.TopRight.x, startX + x * stepX) + Random.value * 0.2f - 0.1f;
+                float yCoord = Mathf.Lerp(gs.BottomLeft.y, gs.TopRight.y, startY + y * stepY) + Random.value * 0.2f - 0.1f;
                 options.Add(new Vector2(xCoord, yCoord));
             }
         }
